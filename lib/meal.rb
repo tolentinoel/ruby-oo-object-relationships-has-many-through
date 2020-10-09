00965ca0370e46c1bfda5915f4d2596fc687d7b5
tolentinoel <rckrleyn@gmail.com>
2020-10-09 09:07:38 -0700

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,22 @@
 class Meal
 
+  attr_accessor :waiter, :customer, :total, :tip
+
+  # know all the details of each meal instance. 
+  #also includes total cost and the tip (default 0)
+  #but also who the customer and waiter were for each meal.
+  @@all = [] 
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
